ebe66ec345bec7d1167c76bdcb1392cc3b25b7ca
Stefan Radny <stefanradny13@gmail.com>
2023-10-03 17:28:17 +0200

    Converted json to array of object and renamed ThemeModel to Colordescription

---
+++ ThemerCore/Colordescription.cs
@@ -0,0 +1,6 @@
+
+namespace ThemerCore;
+public class Colordescription{
+    public string name {get; set;} = string.Empty;
+    public string hex { get; set; } = string.Empty;
+}
--- ThemerCore/ThemeModel.cs
+++
@@ -1,6 +0,0 @@
-public class ThemeModel{
-   public Colordescription colordescription { get; set; }
-}
-public class Colordescription{
-    public string hex { get; set; }
-}
